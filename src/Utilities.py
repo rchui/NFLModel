138eb06e1c1ff2a84fddb621cff0dca1e93f789b
rchui <ryan.w.chui@gmail.com>
2017-04-06 19:09:13 -0500

    added dataReader

--- src/Utilities.py
+++ src/Utilities.py
@@ -1,5 +1,19 @@
-class readData(filePath):
-    """ This reads data into NFLModel """
-    with open('filePath') as file:
-        lines = file.readlines()
+import csv
 
+class Utilities(object):
+    def __init__(self):
+        return None
+
+    def dataReader(self, fileName):
+        """Reads in data for NFLModel
+
+        :fileName: name of file to read in
+        :returns: data read in
+
+        """
+        dataDict = {}
+        with open(fileName) as csvFile:
+            reader = csv.reader(csvFile)
+            for row in reader:
+                dataDict[row[0]] = row[1:]
+        return dataDict

893a49fc9c0a5d4bae91fbf367acb373ba5adf51
rchui <ryan.w.chui@gmail.com>
2017-04-06 19:36:24 -0500

    read in all of the data

--- NFLModel.py
+++ NFLModel.py
@@ -1,3 +1,12 @@
-from src.Utilities import readData
+from src.Utilities import DataReader
+
+dataReader = DataReader()
+rushingData = dataReader.statReader("Rushing.csv")
+passingData = dataReader.statReader("Passing.csv")
+kickingData = dataReader.statReader("Kicking.csv")
+returningData = dataReader.statReader("Returning.csv")
+puntingData = dataReader.statReader("Punting.csv")
+
+playData = dataReader.playReader("Percent.csv")
 
 

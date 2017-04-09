36c0e4d6f6d77ae869cf8d7a5f31f5852d9a1434
rchui <ryan.w.chui@gmail.com>
2017-04-09 16:15:21 -0500

    commented out playpredictor and added game startup

--- GameModeler.py
+++ GameModeler.py
@@ -2,27 +2,24 @@ import sys
 import random
 from src.Utilities import DataReader
 from src.Team import Team
+from src.Game import Game
 
 def printStats(name1, name2):
     """ Print the state of the play predictor
 
     @params:
-        :name: name of the team
+        :name: name of the team1
         :down: down the play is on
         :distance: distance to go
         :field: position on the field
     """
-    print(name)
-    print(down)
-    print(distance)
-    print(field)
+    print(name1)
+    print(name2)
 
-name = sys.argv[1]
-down = int(sys.argv[2])
-distance = int(sys.argv[3])
-field = int(sys.argv[4])
+name1 = sys.argv[1]
+name2 = sys.argv[2]
 
-# Read in team stats
+# Read in team1 stats
 dataReader = DataReader()
 rushingData = dataReader.statReader("Rushing.csv")
 passingData = dataReader.statReader("Passing.csv")
@@ -35,21 +32,27 @@ downData = dataReader.statReader("Down.csv")
 playData = dataReader.playReader("Percent.csv")
 
 # Build Team
-team = Team(name)
-team.buildTeam(rushingData, passingData, kickingData, returningData, puntingData, downData, playData)
+team1 = Team(name1)
+team1.buildTeam(rushingData, passingData, kickingData, returningData, puntingData, downData, playData)
+
+team2 = Team(name2)
+team2.buildTeam(rushingData, passingData, kickingData, returningData, puntingData, downData, playData)
+
+game = Game(team1, team2)
+game.startGame()
 
 # Choose a play
-play = team.nextPlay(down, distance, field)
-if play == "fg":
-    print("Attempting Field Goal: ")
-    if team.fgComp / 100 > random.random():
-        print(str(100 - field + 17) + " yard field goal completed\n")
-    else:
-        print(str(100 - field + 17) + " field goal missed\n")
-elif play == "punt":
-    print("Punting: ")
-    puntDeviation = (team.puntLong - team.puntYA) / 3
-    puntDistance = team.puntYA - random.randint(0, 3) * puntDeviation + random.randint(0, 3) * puntDeviation
-    print("Punted " + str(int(round(puntDistance))) + " yards\n")
-else:
-    print(str(int(play)) + " yards\n")
+# play = team1.nextPlay(down, distance, field)
+# if play == "fg":
+    # print("Attempting Field Goal: ")
+    # if team1.fgComp / 100 > random.random():
+        # print(str(100 - field + 17) + " yard field goal completed\n")
+    # else:
+        # print(str(100 - field + 17) + " field goal missed\n")
+# elif play == "punt":
+    # print("Punting: ")
+    # puntDeviation = (team1.puntLong - team1.puntYA) / 3
+    # puntDistance = team1.puntYA - random.randint(0, 3) * puntDeviation + random.randint(0, 3) * puntDeviation
+    # print("Punted " + str(int(round(puntDistance))) + " yards\n")
+# else:
+    # print(str(int(play)) + " yards\n")
